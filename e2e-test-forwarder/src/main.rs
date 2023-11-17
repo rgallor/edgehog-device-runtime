140e1486c0bae5fcf87afcd7ce221a21c8c7991d
rgallor <riccardo.gallo@secomind.com>
2023-11-17 18:21:57 +0100

    refactor(log): removed useless logs
    Signed-off-by: rgallor <riccardo.gallo@secomind.com>

--- e2e-test-forwarder/src/main.rs
+++ e2e-test-forwarder/src/main.rs
@@ -13,7 +13,7 @@ async fn main() {
     let mut js = JoinSet::new();
 
     js.spawn(con_manager(
-        "ws://localhost:4000/device/websocket?session=abcd".to_string(),
+        "ws://kaiki.local:4000/device/websocket?session=abcd".to_string(),
     ));
 
     while let Some(res) = js.join_next().await {

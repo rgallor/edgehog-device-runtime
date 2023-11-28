11ae5841652542ce926bbbfe864e909ea88950dd
Francesco Noacco <francesco.noacco@secomind.com>
2023-11-28 15:51:45 +0100

    feat(e2e-test-forwarder): add command line arguments using clap
    adds the ability to customize connection parameters
    at runtime, including
    - host
    - port
    - session token
    
    Signed-off-by: Francesco Noacco <francesco.noacco@secomind.com>

--- e2e-test-forwarder/src/main.rs
+++ e2e-test-forwarder/src/main.rs
@@ -1,20 +1,34 @@
 // Copyright 2023 SECO Mind Srl
 // SPDX-License-Identifier: Apache-2.0
 
+use clap::Parser;
 use tokio::task::JoinSet;
 use tracing::info;
+
+#[derive(Parser, Debug)]
+struct Cli {
+    /// Host address of the forwarder server
+    #[arg(long, short = 'H')]
+    host: String,
+    /// Port of the forwarder server
+    #[arg(short, long, default_value_t = 4000)]
+    port: u16,
+    /// Session token
+    #[arg(short, long)]
+    token: String
+}
 
 #[tokio::main]
 async fn main() {
     use edgehog_forwarder::test_utils::con_manager;
+    let Cli{host, port, token} = Cli::parse();
 
     tracing_subscriber::fmt::init();
 
+    let url = format!("ws://{host}:{port}/device/websocket?session={token}");
     let mut js = JoinSet::new();
 
-    js.spawn(con_manager(
-        "ws://kaiki.local:4000/device/websocket?session=abcd".to_string(),
-    ));
+    js.spawn(con_manager(url));
 
     while let Some(res) = js.join_next().await {
         info!("{res:?}");
